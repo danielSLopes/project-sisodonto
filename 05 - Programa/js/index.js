b4364c4b13e4280e29c6915121b8103fa2f268c6
Cristiane Ferreira <cristianedeferreira@gmail.com>
2017-06-22 20:23:19 -0300

    atualizando programa

---
+++ 05 - Programa/js/index.js
@@ -0,0 +1,24 @@
+start();
+
+function start() {
+    var buttonAdd = document.getElementById('adicionar');
+    buttonAdd.onclick = function() {
+        addNewValue();
+    }
+}
+
+function addNewValue() {
+    var inputProduto = document.getElementById('p1');
+    var inputQuantidade = document.getElementById('q1');
+    if (inputProduto.value.trim() == '' || inputQuantidade.value.trim() == '') {
+        alert('Preencha os campos!')
+    } else {
+        var item = document.createElement('option');
+        var list = document.getElementById('lista');
+        list.appendChild(item);
+
+        var texto = document.createTextNode('Quantidade: ' + inputQuantidade.value + ' - Produto: ' + inputProduto.value);
+        item.appendChild(texto);
+
+    }
+}
